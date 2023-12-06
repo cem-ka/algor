9a2813ba41cae5973999c8d06a03f8c15a0327bb
cem-ka <153112438+cem-ka@users.noreply.github.com>
2023-12-06 19:54:39 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Я устала, пока разбиралась с этими репозиториями и очень надеюсь, что сделала всё правильно");
+    }
+}
